4150dee1ad892d5d79e2faec2ed02feef49c3b11
Dalan Cox <dalanjcox@gmail.com>
2022-09-27 20:34:37 -0600

    api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
 app.get("/api", (req, res) => {
     res.json({"users": ["john", "sally", "alyssa", "dalan"]})
 })
