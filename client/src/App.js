d4759f9526b5f32c519dce1501b3f7ce3f6d88a7
Dalan Cox <dalanjcox@gmail.com>
2022-09-29 19:19:33 -0600

    styled with bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import uuid from 'react-uuid';
 
 function App() {
   const [name, setName] = useState("");
@@ -12,7 +17,7 @@ function App() {
       method: "post",
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
-        "id": "1",
+        "id": uuid(),
         "name": name,
         "age": age,
         "height": height
@@ -30,35 +35,52 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <div>
+    <div className="App" style={{
+      width: "100%",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
 
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={name}
-            placeholder="Name"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            value={age}
-            placeholder="Age"
-            onChange={(e) => setAge(e.target.value)}
-          />
-          <input
-            type="text"
-            value={height}
-            placeholder="Height"
-            onChange={(e) => setheight(e.target.value)}
-          />
-
-          <button type="submit">Submit</button>
-
-        </form>
-      </div>
+      <Card style={{ width: '15rem' }}>
+        <Card.Body>
+          <Card.Title>
+            Create User
+          </Card.Title>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={name}
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+                style={{marginTop: '1rem', width: '100%'}}
+              />
+              <input
+                type="text"
+                value={age}
+                placeholder="Age"
+                onChange={(e) => setAge(e.target.value)}
+                style={{marginTop: '1rem', width: '100%'}}
+              />
+              <input
+                type="text"
+                value={height}
+                placeholder="Height"
+                onChange={(e) => setheight(e.target.value)}
+                style={{marginTop: '1rem', width: '100%'}}
+              />
+              <Button 
+                  variant="primary"
+                  type="submit"
+                  style={{marginTop: '1rem'}}
+              >
+                Submit</Button>
+            </form>
+          </div>
+          </Card.Body>
+      </Card>
     </div>
   );
 }
